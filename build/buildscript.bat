50ec1d4d5189c703f9cb11c918a1cc19b85c0c62
Fionnuala Carr <fcarr@kx.com>
2018-06-29 10:06:36 +0100

    test

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -1,16 +1,11 @@
 call "build\getkdb.bat" || goto :error
 
-
 if not defined QLIC_KC (
  goto :nokdb
 )
 
 
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
-mkdir embedpy
-cd embedpy
-echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
-cd ..
 exit /b 0
 
 :error
--- build/getkdb.bat
+++ build/getkdb.bat
@@ -6,4 +6,3 @@ certutil -decode q\kc.lic.enc q\kc.lic
 set QHOME=%cd%\q
 set PATH=%QHOME%\w64;%PATH%
 echo .z.K | q -q
-\\
