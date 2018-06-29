bd6c713e62db8bfc0494a4b990a9ca91ff7e7d16
Fionnuala Carr <fcarr@kx.com>
2018-06-29 09:16:32 +0100

    tests

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -1,14 +1,16 @@
 call "build\getkdb.bat" || goto :error
+
+
 if not defined QLIC_KC (
  goto :nokdb
 )
+
 
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
 mkdir embedpy
 cd embedpy
 echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
 cd ..
-echo p)print('embedpy runs') | q -q || goto :error
 exit /b 0
 
 :error
